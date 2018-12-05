81e3ae7fba6bcf961430b205637e447d12435f67
mattwind <mattwind@gmail.com>
2018-12-05 05:04:16 -0800

    no queued skip

--- rss-bot/libs/discord.sh
+++ rss-bot/libs/discord.sh
@@ -5,15 +5,19 @@ source conf/config
 count=0
 sql=$( $libs/db_crud.sh queued )
 
-while IFS="|" read -r wid md5sum url 
-do
-  webhook=`./$libs/db_crud.sh getWebhook "$wid" 2>&1`
-	# send to discord
-	postDataJson="{\"username\":\"$botname\",\"content\":\"$url\"}"
-  curl -s -H "Content-Type: application/json" -X POST -d ${postDataJson} $webhook
-	# update db status
-	update=`./$libs/db_crud.sh processed "$md5sum" 2>&1`
-  count=$((count+1))
-done <<< $sql
+if [ "$sql" != "" ]; then
+
+  while IFS="|" read -r wid md5sum url 
+  do
+    webhook=`./$libs/db_crud.sh getWebhook "$wid" 2>&1`
+    # send to discord
+    postDataJson="{\"username\":\"$botname\",\"content\":\"$url\"}"
+    curl -s -H "Content-Type: application/json" -X POST -d ${postDataJson} $webhook
+    # update db status
+    update=`./$libs/db_crud.sh processed "$md5sum" 2>&1`
+    count=$((count+1))
+  done <<< $sql
+
+fi
 
 echo $count
